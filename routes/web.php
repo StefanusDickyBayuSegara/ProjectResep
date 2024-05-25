2507e1357753fd727d08cecff052b5ed065ec2a4
StefanusDickyBayuSegara <bayusegara1011@gmail.com>
2024-05-25 11:14:33 +0700

    menambahkan Sing-up

--- routes/web.php
+++ routes/web.php
@@ -2,10 +2,15 @@
 
 use Illuminate\Support\Facades\Route;
 
+
 Route::get('/', function () {
     return view('welcome');
 });
 
 Route::get('/standby', function () {
     return view('Standby');
+});
+
+Route::get('/Sing-up', function () {
+    return view('Sing-up');
 });
